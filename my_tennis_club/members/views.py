5b7f66708a307d052ea62a4b93c3b105ce3c1c24
Carsano <damien.fabiano@protonmail.com>
2025-03-11 21:47:42 +0100

    Modify view

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 
 def members(request):
-    return HttpResponse("Hello world!")
+    template = loader.get('myfirst.html')
+    return HttpResponse(template.render())
